cfcaf03d4f35d0377f51356040f02277fc26859e
Alvin Accad <alvinaccad5@gmail.com>
2022-08-01 08:57:32 -0700

    completed assessment

--- code-challenges.test.js
+++ code-challenges.test.js
@@ -14,6 +14,23 @@
 
 // a) Create a test with an expect statement using the variable provided.
 
+describe("output", () => {
+  const people = [
+    { name: "ford prefect", occupation: "a hitchhiker" },
+    { name: "zaphod beeblebrox", occupation: "president of the galaxy" },
+    { name: "arthur dent", occupation: "a radio employee" }
+  ]
+    
+  it("takes in an array of objects and returns an array with a sentence about each person with their name capitalized", () => {
+    expect(output(people)).toEqual([
+      'Ford Prefect is a hitchhiker.',
+      'Zaphod Beeblebrox is president of the galaxy.',
+      'Arthur Dent is a radio employee.'
+    ])
+  })
+})
+
+
 const people = [
   { name: "ford prefect", occupation: "a hitchhiker" },
   { name: "zaphod beeblebrox", occupation: "president of the galaxy" },
@@ -25,9 +42,77 @@ const people = [
 // b) Create the function that makes the test pass.
 
 
+// ReferenceError: output is not defined
+
+// Psuedocode:
+// 1. Create a function that takes an array parameter
+// 2. We will use a HOF and use the .map method that takes in the name and occupation
+// 3. Inside the .map method, we will create a new variable called newName and set it to name.split(" ")
+// 4. We will use the return statement to get back the output we are looking for
+//    4a. Use string interpolation for the first item to return. We will want to get the newName[0] for the first name and add .charAt(0).toUpperCase() ...This will make the first letter in the name uppercase
+//    4b. Use the plus symbol and use the newName[0].substring(1) to add the rest of the first name
+// 5. Use a " " and another + symbole to seperate the first name and the last name
+// 6. Use the step 4a and 4b for the last name, but instead of index newName[0], it will be index newName[1]. This will be used for the last name and for it the first letter to be uppercased
+// 7. Type in: is ${occupation}.` This will finish out the output we are looking for.
+
+// Code block below doesn't work
+// const output = people.map(({name, occupation}) => {
+  // return name.charAt(0).toUpperCase() + " is a " + occupation
+  // name = name.charAt(0).toUpperCase() + name.substring(1).toLowerCase()
+  // return name.charAt(0).toUpperCase() + name.slice(1)
+  // return name.split()
+  // return `${name.split(" ")} is a ${occupation}`
+// })
+
+// Code block below doesn't work
+// const output2 = (array) => {
+//   return array.map(({name, occupation}) => {
+//     name.split(" ")
+//     name = name.charAt(0).toUpperCase() + name.substring(1)
+//     let name2 = name.split(" ")
+    
+//     return `${name} is a ${occupation}` + " " + name2[1].charAt(0).toUpperCase() + name2[1].substring(1)
+//   })
+// }
+
+// Below function works
+const output = (array) => {
+    return array.map(({name, occupation}) => {
+      let newName = name.split(" ")
+
+      return `${(newName[0].charAt(0).toUpperCase() + newName[0].substring(1)) + 
+        " " + (newName[1].charAt(0).toUpperCase() + newName[1].substring(1))} is ${occupation}.`
+
+
+    })
+}
+
+console.log(output(people)) // Output: ['Ford Prefect is a hitchhiker.', 'Zaphod Beeblebrox is president of the galaxy.', 'Arthur Dent is a radio employee.']
+
+// //  PASS  ./code-challenges.test.js
+//   output
+//   ✓ takes in an array of objects and returns an array with a sentence about each person with their name capitalized (1 ms)
+
+
+
+
 // --------------------2) Create a function that takes in a mixed data array and returns an array of only the REMAINDERS of the numbers when divided by 3.
 
 // a) Create a test with an expect statement using the variables provided.
+
+describe("mixedData", () => {
+  const hodgepodge1 = [23, "Heyyyy!", 45, -10, 0, "Yo", false]
+  // Expected output: [ 2, 0, -1, 0 ]
+  const hodgepodge2 = [5, "Hola", 43, -34, "greetings", true]
+  // Expected output: [ 2, 1, -1 ]
+  
+  it("takes in a mixed data array and returns an array of only the REMAINDERS of the numbers when divided by 3", () => {
+    expect(mixedData(hodgepodge1)).toEqual([ 2, 0, -1, 0 ])
+    expect(mixedData(hodgepodge2)).toEqual([ 2, 1, -1 ])
+  })
+})
+
+//     ReferenceError: mixedData is not defined
 
 const hodgepodge1 = [23, "Heyyyy!", 45, -10, 0, "Yo", false]
 // Expected output: [ 2, 0, -1, 0 ]
@@ -37,10 +122,50 @@ const hodgepodge2 = [5, "Hola", 43, -34, "greetings", true]
 
 // b) Create the function that makes the test pass.
 
+// Psuedocode: 
+// 1. Create a function called mixedData that takes in 1 parameter
+// 2. Create a new array and set it to empty
+// 3. Create a for loop to iterate through each item in the array
+// 4. Use an if conditional to check if the typeof array[i] is strictly equal to a number
+//    4a. If it is, then we can use the newArray.push method to insert the array[i] % 3. This will put the expected remained number we are looking for.
+// 5. After exiting the for loop, return the newArray
+
+const mixedData = (array) => {
+  let newArray = []
+
+  for(let i=0; i<array.length; i++) {
+      if(typeof array[i] === "number") {
+          newArray.push(array[i] % 3)
+      }
+  }
+  return newArray
+}
+
+console.log(mixedData(hodgepodge1)) // Output: [ 2, 0, -1, 0 ]
+console.log(mixedData(hodgepodge2)) // Output: [ 2, 1, -1 ]
+
+// PASS  ./code-challenges.test.js
+// mixedData
+//   ✓ takes in a mixed data array and returns an array of only the REMAINDERS of the numbers when divided by 3 (1 ms)
+
 
 // --------------------3) Create a function that takes in an array of numbers and returns the sum of all the numbers cubed.
 
 // a) Create a test with an expect statement using the variables provided.
+
+describe("sumOfCubed", () => {
+  const cubeAndSum1 = [2, 3, 4]
+  // Expected output: 99
+  const cubeAndSum2 = [0, 5, 10]
+  // Expected output: 1125
+
+  it("takes in an array of numbers and returns the sum of all the numbers cubed", () => {
+    expect(sumOfCubed(cubeAndSum1)).toEqual(99)
+    expect(sumOfCubed(cubeAndSum2)).toEqual(1125)
+  })
+})
+
+//     ReferenceError: sumOfCubed is not defined
 
 const cubeAndSum1 = [2, 3, 4]
 // Expected output: 99
@@ -49,3 +174,31 @@ const cubeAndSum2 = [0, 5, 10]
 
 
 // b) Create the function that makes the test pass.
+
+// 2 = 8
+// 3 = 27
+// 4 = 64
+// = 99
+
+// Psuedocode:
+// 1. Create a variable called sumOfCubed and it takes in 1 parameter called array
+// 2. Create a sum variable and set it to equal zero
+// 3. Create a for loop to iterate through each item in the array
+// 4. Create logic by setting the sum to += each array[i] item and multiple array[i] by itself three times total
+// 5. Exit the for loop and return the sum
+
+const sumOfCubed = (array) => {
+  let sum = 0
+
+  for(let i=0; i<array.length; i++) {
+    sum += array[i] * array[i] * array[i]
+  }
+  return sum
+}
+
+console.log(sumOfCubed(cubeAndSum1)) // Output: 99
+console.log(sumOfCubed(cubeAndSum2)) // Output: 1125
+
+// PASS  ./code-challenges.test.js
+// sumOfCubed
+//   ✓ takes in an array of numbers and returns the sum of all the numbers cubed (1 ms)
--- interview-questions.md
+++ interview-questions.md
@@ -8,36 +8,56 @@ Then, research the question to expand on your answer. Even if you feel you have 
 
 1. As a developer, I am creating a Rails application with a model called Cohort that has_many students, but OOPS! I forgot to add the foreign key. How can I fix this mistake? What is the name of the foreign key? Would the foreign key be on the Cohort model or the Student model?
 
-Your answer:
+Your answer: You can fix this mistake by adding the foreign key into the database by doing a change to the migration. The name of the foreign key would cohort_id and it would be on the Student model, because you want to compare the cohort to the Student model.
 
-Researched answer:
+Researched answer: Creating a migration will help adding the foreign key. The add_column command can be used to add the foreign key. A Cohort has_many students and a student belongs_to a cohort. The foreign key will go to the cohort model and be called student_id
 
 2. Which RESTful routes must always be passed params? Why?
 
-Your answer:
+Your answer: Params are used to validate what goes into the database. Create and update must be passed params.
 
-Researched answer:
+Researched answer: Strong params provide control on what goes into the database. Strong params can choose what goes in by the create and update RESTful route.
 
 3. Name three rails generator commands. What is created by each?
 
 Your answer:
+- rails db:create - A database is created
+- rails server or rails s - It creates a local host to view the website
+- rails new file_name - It creates a new Ruby on Rails application
 
 Researched answer:
+- rails generate controller Main - Creates a controller, which is used in the MVC and the controller is used to interact with the model and view.
+- rails generate rspec:install - Used to install rspec onto the Rails application
+- rails generate model Student name:string cohort:string - Creates the model for the application
+
 
 4. Consider the Rails routes below. What is the name of the controller method that would be called by each route? What action would each of the controller methods perform?
 
-action: "GET" location: /students
+action: "GET" location: /students - Index (show all the items in the database)
 
-action: "POST" location: /students
+action: "POST" location: /students - Create (submits data to the database)
 
-action: "GET" location: /students/new
+action: "GET" location: /students/new - New (adds data to the database)
 
-action: "GET" location: /students/2
+action: "GET" location: /students/2 - Show (looks for the item #2 in the database)
 
-action: "GET" location: /students/2/edit
+action: "GET" location: /students/2/edit - Edit (edit the data in the item #2 in the database)
 
-action: "PATCH" location: /students/2
+action: "PATCH" location: /students/2 - Update (modify item #2 in the database)
 
-action: "DELETE" location: /students/2
+action: "DELETE" location: /students/2 - Destroy (delete the item #2 in the database)
+
+The controller method would be called Student
 
 5. As a developer, you are making an application to manage your to do list. Create 10 user stories that will help you get your application started. Read more about [user stories](https://www.atlassian.com/agile/project-management/user-stories).
+
+As a developer, I need to create my Rails application
+As a developer, I need to create a PostgreSQL database
+As a developer, I need to create a ToDo model
+As a developer, I need to do a db:migrate
+As a developer, I need to create a landing/index page
+As a developer, I need to create a way to create to-do tasks
+As a developer, I need to create a way to display to-do tasks
+As a developer, I need to create a way to edit/update the to-do tasks
+As a developer, I need to create a way to delete the to-do tasks
+As a developer, I need to add styling to the application
